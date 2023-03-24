4a22fe4e78c93d557c7e6f061c06c9d44680fdbb
LuisGabrielMascarenhas <luis.santos614@etec.sp.gov.br>
2023-03-23 22:09:31 -0300

    Ativando o Botão de Click

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
     //declarando o objeto
@@ -23,6 +24,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                Toast.makeText(getApplicationContext(),"Cliquei",Toast.LENGTH_SHORT).show();
             }
         });
     }

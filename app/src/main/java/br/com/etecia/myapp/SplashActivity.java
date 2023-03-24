5a837e41f172c46b07f9f56048d95c847d2e53ec
LuisGabrielMascarenhas <luis.santos614@etec.sp.gov.br>
2023-03-23 22:06:10 -0300

    Ativando o Botão de Click

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,13 +2,28 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
+    //declarando o objeto
+    Button btnAbrirLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+        //Apresentando o xml para o java
+        btnAbrirLogin = findViewById(R.id.btnAbrirLogin);
+        //criar a ação de clique no botão
+        btnAbrirLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
     }
 }
